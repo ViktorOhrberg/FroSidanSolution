394d8e052d5e4c2ea44148b905479dc2d1925c55
ViktorOhrberg <65067829+ViktorOhrberg@users.noreply.github.com>
2021-03-29 13:43:16 +0200

    Added properties to product table

--- FrosidanDB/dbo/Tables/Products.sql
+++ FrosidanDB/dbo/Tables/Products.sql
@@ -1,5 +1,14 @@
 ﻿CREATE TABLE [dbo].[Products]
 (
 	[Id] INT NOT NULL PRIMARY KEY Identity,
-	[Name] nvarchar(50) Not NUll
+	[Name] nvarchar(100) Not null,
+	[Price] int not null,
+	[TempPrice] int null,
+	[Balance] int not null,
+	[ImgRef] nvarchar(100) null,
+	[ThumbRef] nvarchar(100) null,
+	[Description] nvarchar(max) Null,
+	[Category] nvarchar(100) not null,
+	[SubCategory] nvarchar(100) null,
+	[Quantity] int not null
 )
