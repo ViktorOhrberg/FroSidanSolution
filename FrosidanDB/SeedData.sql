e22c9b5cb90a67944435c29c1f6327cea7a1ec98
HenrikSundin <hmsundin@gmail.com>
2021-03-29 15:40:24 +0200

    Tillagt sql query i ny fil seeddata

---
+++ FrosidanDB/SeedData.sql
@@ -0,0 +1,84 @@
+﻿insert into Products ([Quantity], [SubCategory], [Category], [Description], [ThumbRef], [ImgRef], [Balance], [TempPrice], [Price], [Name])
+values
+(6, 'Chili', 'Frön', 'Cabai Burung Ungu är en sällsynt och otroligt vacker chili som kommer från Malaysia där namnet betyder "Purple Bird Pepper".
+En lilafärgad Capsicum frutescens är mycket märkligt och chilin antas vara en korsning mellan frutescens och baccatum.
+Plantorna har ett buskigt växtsätt och blir ofta meterhöga med stora skördar av uppåtväxande 3-5 cm långa färgglada bird peppers.
+
+Cabai Burung Ungu
+Även blad, stam och grenar har lila inslag vilket gör plantan mycket dekorativ.
+Fruktens färg går från grön till lila för att avsluta som orange och djupröd som fullt mogen.
+Smaken har kryddigheten och aromen som är typisk för många asiatiska Bird Peppers och hettan ligger i spannet 30.000-50.000 Scoville.
+Chilin lämpar sig väl för torkning eller infrysning och går bra att äta i alla färgvarianter.
+
+Blötlägg frön i ljummet vatten något dygn innan sådd och så på 5 mm djup i såjord.
+(För bästa möjliga groddresultat rekommenderas blötläggning i en lösning av kaliumnitrat)
+Håll jorden fuktig men inte blöt.
+Chilifrö gror bäst vid 25-30°C och behöver i regel 10-30 dagar till grodd.
+Plantera om efterhand som plantan växer och rötterna utvecklas.
+
+Trivs i soligt fönster, uterum, växthus, utomhus i soligt och skyddat läge under sommaren.
+Vid odling under vinterhalvåret krävs i regel någon form av växtbelysning.
+Hjälp blommorna med pollinering genom att skaka plantan eller med en liten pensel.
+Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge.
+', 'cabai_burung_unguwebsmall.jpg', 'cabai_burung_unguweb.jpg', 10, null, 28, 'Cabai Burung Ungu'),
+
+(6, 'Chili', 'Frön', 'Brain Strain Yellow är en explosivt het 7 Pot-variant från ön Trinidad utanför Venezuelas kust.
+Namnet kommer från chilins likhet med en mänsklig hjärna.
+
+Plantorna blir ca 140 cm höga och har mycket stora blad som liknar tobaksblad.
+De runda och gula frukterna blir ofta stora som golfbollar med en knottrig yta.
+Smaken är söt och fruktig i några sekunder innan den extrema hettan slår till med full kraft.
+7 Pot Brain Strain Yellow har en styrka som kan uppgå till strax över en miljon Scoville.
+Denna gula variant har egenheten att ibland ge släta frukter i början av säsongen och knottriga mot slutet
+
+Blötlägg frön i ljummet vatten något dygn innan sådd och så på 5 mm djup i såjord.
+(För bästa möjliga groddresultat rekommenderas blötläggning i en lösning av kaliumnitrat)
+Håll jorden fuktig men inte blöt.
+Behöver ha varm jord, 25-30°C och 10-40 dagar för att gro.
+Plantera om efterhand som plantan växer och rötterna utvecklas
+
+Trivs i soligt fönster, uterum, växthus, utomhus i soligt och skyddat läge under sommaren.
+Vid odling under vinterhalvåret krävs i regel någon form av växtbelysning.
+Hjälp blommorna med pollinering genom att skaka plantan eller med en liten pensel.
+Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge', 'brain_strain_yellow-web_1small.jpg', 'brain_strain_yellow-web_1.jpg', 10, null, 29, '7 Pot Brain Strain Yellow'),
+(6, 'Chili', 'Frön', 'White  Jolokia är en variant av Bhut Jolokia som ursprungligen kommer från Indien och Bangladesh.
+Plantan blir meterhög och chilins smak och doft är fruktigare och mer aromatisk än röd Bhut Jolokia.
+White Bhut Jolokia är knubbig och har rejält tjocka väggar med en hetta som ligger en strax under den röda Bhutens hetta på en miljon Scoville.
+Fruktens färg går från grönt till cremegul/vit vid mognad och chilin blir ca 5-8 cm lång med en slät yta.
+Detta är en idealisk chili för att torka och mala till ett vitt chilipulver med hög hetta.
+
+
+Blötlägg frön i ljummet vatten något dygn innan sådd och så på 5 mm djup i såjord.
+(För bästa möjliga groddresultat rekommenderas blötläggning i en lösning av kaliumnitrat)
+Håll jorden fuktig men inte blöt.
+Chilifrö gror bäst vid 25-30°C och behöver i regel 10-30 dagar till grodd.
+Plantera om efterhand som plantan växer och rötterna utvecklas.
+
+Trivs i soligt fönster, uterum, växthus, utomhus i soligt och skyddat läge under sommaren.
+Vid odling under vinterhalvåret krävs i regel någon form av växtbelysning.
+Hjälp blommorna med pollinering genom att skaka plantan eller med en liten pensel.
+Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge', 'White_Bhutweb_1small.jpg', 'White_Bhutweb_1.jpg', 10, null, 24, 'Bhut Jolokia White'),
+
+(6, 'Chili', 'Frön', 'Cabai Burung Ungu är en sällsynt och otroligt vacker chili som kommer från Malaysia där namnet betyder "Purple Bird Pepper".
+En lilafärgad Capsicum frutescens är mycket märkligt och chilin antas vara en korsning mellan frutescens och baccatum.
+Plantorna har ett buskigt växtsätt och blir ofta meterhöga med stora skördar av uppåtväxande 3-5 cm långa färgglada bird peppers.
+
+
+Cabai Burung Ungu
+Även blad, stam och grenar har lila inslag vilket gör plantan mycket dekorativ.
+Fruktens färg går från grön till lila för att avsluta som orange och djupröd som fullt mogen.
+Smaken har kryddigheten och aromen som är typisk för många asiatiska Bird Peppers och hettan ligger i spannet 30.000-50.000 Scoville.
+Chilin lämpar sig väl för torkning eller infrysning och går bra att äta i alla färgvarianter.
+
+Blötlägg frön i ljummet vatten något dygn innan sådd och så på 5 mm djup i såjord.
+(För bästa möjliga groddresultat rekommenderas blötläggning i en lösning av kaliumnitrat)
+Håll jorden fuktig men inte blöt.
+Chilifrö gror bäst vid 25-30°C och behöver i regel 10-30 dagar till grodd.
+Plantera om efterhand som plantan växer och rötterna utvecklas.
+
+Trivs i soligt fönster, uterum, växthus, utomhus i soligt och skyddat läge under sommaren.
+Vid odling under vinterhalvåret krävs i regel någon form av växtbelysning.
+Hjälp blommorna med pollinering genom att skaka plantan eller med en liten pensel.
+Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge.', 'cabai_burung_unguwebsmall.jpg', 'cabai_burung_unguweb.jpg', 10, null, 29, 'Cabai Burung Ungu')
+
+
