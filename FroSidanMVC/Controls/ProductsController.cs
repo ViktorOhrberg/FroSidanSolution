c7eaeb423cde357dd8148cbb759bee6a0d33bd3c
ViktorOhrberg <65067829+ViktorOhrberg@users.noreply.github.com>
2021-03-29 09:38:10 +0200

    Add project files.

---
+++ FroSidanMVC/Controls/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using FroSidanMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FroSidanMVC.Controls
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductsService service;
+
+        public ProductsController(ProductsService service)
+        {
+            this.service = service;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Content("tjohej world");
+            //return View();
+        }
+    }
+}
