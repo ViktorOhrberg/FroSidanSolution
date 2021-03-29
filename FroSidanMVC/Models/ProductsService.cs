ca6584a2446958c8b8d662904eaf90e51c1ea0b5
ViktorOhrberg <65067829+ViktorOhrberg@users.noreply.github.com>
2021-03-29 10:15:43 +0200

    Tillagda metoder för kundkorg

--- FroSidanMVC/Models/ProductsService.cs
+++ FroSidanMVC/Models/ProductsService.cs
@@ -7,5 +7,30 @@ namespace FroSidanMVC.Models
 {
     public class ProductsService
     {
+        List<int> shoppingCart; // Ska vara av typen product
+
+        public ProductsService(List<int> shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public void AddToCart( /*ProduktId*/ )
+        {
+            shoppingCart.Add(1);
+        }
+        public void RemoveFromCart( /*ProduktId*/ )
+        {
+            //shoppingCart
+            //    .FirstOrDefault(/*p => p.ID == ProductID*/)
+            //    .Remove();
+        }
+        public int QuantityInCart( /*ProduktId*/ )
+        {
+            int quantity = 1;
+            //shoppingCart
+            //    .Where(p => p.ID == ProduktId)
+            //    .Count();
+            return quantity;
+        }
     }
 }
