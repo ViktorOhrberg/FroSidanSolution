53d0b6e13e28193d2fe6bc7fb67369dc3c49d5a8
Emilost91 <emil.91@hotmail.com>
2021-03-30 15:45:23 +0200

    Skapade Orders databasen.

---
+++ FrosidanDB/dbo/Tables/Orders.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity,
+	[Innehåll] nvarchar(max) not null,
+	[Datum] datetime not null,
+	[Leveransadress] nvarchar(100) not null,
+	[Kund-ID] nvarchar(450) references AspNetUsers(Id) not null,
+	[Betalningsmetod] nvarchar(50) not null
+)
