00afc9a40a06929ed97fb3df6c955397ac5c2d8a
ViktorOhrberg <65067829+ViktorOhrberg@users.noreply.github.com>
2021-03-31 08:49:52 +0200

    Uppdateringar för utcheckningsflödet

---
+++ FroSidanMVC/Models/ViewModels/Products/CheckoutVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FroSidanMVC.Models.ViewModels.Products
+{
+    public class CheckoutVM
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string Zip { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
