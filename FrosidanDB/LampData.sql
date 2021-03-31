0553bab2deded3f0b63cd605a1f66a4185dfa2b2
Emilost91 <emil.91@hotmail.com>
2021-03-31 10:28:41 +0200

    Skapade LampData i databasen.

---
+++ FrosidanDB/LampData.sql
@@ -0,0 +1,79 @@
+﻿insert into Products ([Quantity], [SubCategory], [Category], [Description], [ThumbRef], [ImgRef], [Balance], [TempPrice], [Price], [Name])
+values
+(
+	6,
+	'LED-ramp',
+	'Lampor',
+	'Smart växtbelysning som tar liten plats, ej fristående. Kopplas ihop med Växtbelysning LED No 1, 15W. Kan hängas i ställning eller skruvas under ett hyllplan.',
+	'vaxtbelysning-led-no2-60cm-15-w-5568small',
+	'vaxtbelysning-led-no2-60cm-15-w-5568',
+	10,
+	null,
+	329,
+	'Växtbelysning LED No.2 60 cm 15W'
+),
+
+(
+	6,
+	'LED-lampa',
+	'Lampor',
+	'Används för uppdrivning av plantor, som ljustillskott under de mörka årstiderna, blomningskontroll, eller inomhus där solljus saknas helt.',
+	'vaxtlampa-odla-standardsmall',
+	'vaxtlampa-odla-standard',
+	10,
+	null,
+	399,
+	'Växtlampa Odla 6W, 60grader'
+),
+
+(
+	6,
+	'Lysrör',
+	'Lampor',
+	'En nätt, smal och lättplacerad växtlampa med den bästa typ av belysning du kan ge dina växter.',
+	'vaxtlampa-lysror-t5-uno-vinkelsmall',
+	'vaxtlampa-lysror-t5-uno-vinkel',
+	10,
+	null,
+	489,
+	'Växtlampa LightWave T5 UNO 24 W'
+),
+
+(
+	6,
+	'Bordslampa',
+	'Lampor',
+	'Lampan fästs enkelt med klämman på bordskanten eller i bokhyllan. Kan placeras ovanifrån, från sidan och underifrån. Lampan har tre olika inställningar för ljus som väljs med hjälp av knapparna på strömbrytaren.',
+	'led-grow-desk-light-vaxtlampa-med-armsmall',
+	'led-grow-desk-light-vaxtlampa-med-arm',
+	10,
+	null,
+	499,
+	'Orion Grow Light'
+),
+
+(
+	6,
+	'LED-lampa',
+	'Lampor',
+	'Primula Grow har en ljussammansättning som är optimal för att odla växter inomhus och för att dra upp plantor från frö och sticklingar.',
+	'vaxtbelysning-plant-light-primula-antracit-20wsmall',
+	'vaxtbelysning-plant-light-primula-antracit-20w',
+	10,
+	null,
+	679,
+	'Plant Light Primula Grow, antracit, 20W'
+),
+
+(
+	6,
+	'LED-lampa',
+	'Lampor',
+	'Primula Grow har en ljussammansättning som är optimal för att odla växter inomhus och för att dra upp plantor från frö och sticklingar.',
+	'growspot-7-watt-e27small',
+	'growspot-7-watt-e27',
+	10,
+	null,
+	279,
+	'LED-lampa Growspot 7W E27-sockel röd/blå'
+)
