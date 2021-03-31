9bb75f72bd2945b6d3e16fdef34aa7d7ba8c04c0
HenrikSundin <hmsundin@gmail.com>
2021-03-31 10:04:13 +0200

    Ändrat seeddata sqlen så den inte har en dublett

--- FrosidanDB/SeedData.sql
+++ FrosidanDB/SeedData.sql
@@ -59,16 +59,11 @@ Vid odling under vinterhalvåret krävs i regel någon form av växtbelysning.
 Hjälp blommorna med pollinering genom att skaka plantan eller med en liten pensel.
 Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge', 'White_Bhutweb_1small.jpg', 'White_Bhutweb_1.jpg', 10, null, 24, 'Bhut Jolokia White'),
 
-(6, 'Chili', 'Frön', 'Cabai Burung Ungu är en sällsynt och otroligt vacker chili som kommer från Malaysia där namnet betyder "Purple Bird Pepper".
-En lilafärgad Capsicum frutescens är mycket märkligt och chilin antas vara en korsning mellan frutescens och baccatum.
-Plantorna har ett buskigt växtsätt och blir ofta meterhöga med stora skördar av uppåtväxande 3-5 cm långa färgglada bird peppers.
-
-
-Cabai Burung Ungu
-Även blad, stam och grenar har lila inslag vilket gör plantan mycket dekorativ.
-Fruktens färg går från grön till lila för att avsluta som orange och djupröd som fullt mogen.
-Smaken har kryddigheten och aromen som är typisk för många asiatiska Bird Peppers och hettan ligger i spannet 30.000-50.000 Scoville.
-Chilin lämpar sig väl för torkning eller infrysning och går bra att äta i alla färgvarianter.
+(6, 'Chili', 'Frön', 'Aji Amarillo är en chili från Peru som i Sydamerika ofta torkas och används för framställning av chilipulver.
+Plantorna blir ca 90 cm höga och bär rikligt med koniskt formade chilis.
+Chilin blir 10-12 cm lång och går från grönt till gult/brandgult vid mognad.
+Styrkan är medelhet och ligger omkring 50.000 Scoville.
+Smaken är rik och aromatisk och passar utmärkt till fiskrätter och skaldjur.
 
 Blötlägg frön i ljummet vatten något dygn innan sådd och så på 5 mm djup i såjord.
 (För bästa möjliga groddresultat rekommenderas blötläggning i en lösning av kaliumnitrat)
@@ -79,6 +74,6 @@ Plantera om efterhand som plantan växer och rötterna utvecklas.
 Trivs i soligt fönster, uterum, växthus, utomhus i soligt och skyddat läge under sommaren.
 Vid odling under vinterhalvåret krävs i regel någon form av växtbelysning.
 Hjälp blommorna med pollinering genom att skaka plantan eller med en liten pensel.
-Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge.', 'cabai_burung_unguwebsmall.jpg', 'cabai_burung_unguweb.jpg', 10, null, 29, 'Cabai Burung Ungu')
+Fleråring - Beskäres på hösten - Övervintras inomhus i ljust läge', 'aji_amarillo-websmall.jpg', 'aji_amarillo-web.jpg', 10, null, 29, 'Aji Amarillo')
 
 
