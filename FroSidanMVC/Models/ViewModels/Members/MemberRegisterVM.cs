a854305837889618d2aa2359dc4637eafff67154
Emilost91 <emil.91@hotmail.com>
2021-04-08 09:44:38 +0200

    La till regex i register formuläret.

--- FroSidanMVC/Models/ViewModels/Members/MemberRegisterVM.cs
+++ FroSidanMVC/Models/ViewModels/Members/MemberRegisterVM.cs
@@ -4,9 +4,20 @@ namespace FroSidanMVC.Models
 {
     public class MemberRegisterVM
     {
+        [Required(ErrorMessage = "Ange förnamn")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Ange efternamn")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Ange korrekt epostadress")]
+        [EmailAddress]
+        public string Email { get; set; }
+
         [StringLength(100, ErrorMessage = "Lösenordet måste vara minst 6 tecken långt.", MinimumLength = 6)]
         [Required(ErrorMessage = "Ange ett lösenord")]
         [DataType(DataType.Password)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,15}$", ErrorMessage = "Måste minst innehålla 1 STOR bokstav, 1 liten bokstav samt 1 nummer")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Matchar ej angivet lösenord")]
@@ -16,13 +27,6 @@ namespace FroSidanMVC.Models
         public string PasswordRepeat { get; set; }
 
         public string UserName { get; set; }
-        [Required(ErrorMessage = "Ej korrekt angiven epostadress")]
-        [EmailAddress]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Ange förnamn")]
-        public string FirstName { get; set; }
-        [Required(ErrorMessage = "Ange efternamn")]
-        public string LastName { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string Zip { get; set; }
